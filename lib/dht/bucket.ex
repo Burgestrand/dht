819662c62b1fd16634b953ae61092d31d45fc63f
Kim Burgestrand <kim@burgestrand.se>
2015-08-12 20:05:39 +0200

    Add Bucket #add & #size.

--- lib/dht/bucket.ex
+++ lib/dht/bucket.ex
@@ -1,10 +1,29 @@
 defmodule DHT.Bucket do
-  @type t :: %DHT.Bucket{range: Range.t(DHT.ID.t, DHT.ID.t), members: []}
-  defstruct range: (0..DHT.ID.max), members: []
+  @type t :: %DHT.Bucket{range: Range.t(DHT.ID.t, DHT.ID.t), members: Set.t}
+  defstruct range: (0..DHT.ID.max), members: HashSet.new
 
   @doc "True if ID is within bucket range."
   @spec cover?(DHT.Bucket.t, DHT.ID.t) :: boolean
   def cover?(%DHT.Bucket{range: range}, id) do
     Enum.member?(range, id)
+  end
+
+  @doc "Number of members in the bucket."
+  @spec size(DHT.Bucket.t) :: integer
+  def size(%DHT.Bucket{members: members}) do
+    Set.size(members)
+  end
+
+  @spec add(DHT.Bucket.t, DHT.ID.t) :: {:ok, DHT.Bucket.t}
+  def add(%DHT.Bucket{members: members} = bucket, id) do
+    if cover?(bucket, id) do
+      unless Set.member?(members, id) do
+        {:ok, %{bucket | members: Set.put(members, id)}}
+      else
+        {:error, "#{id} already exists within bucket"}
+      end
+    else
+      {:error, "#{id} is not within #{inspect bucket.range}"}
+    end
   end
 end
--- test/dht/bucket_test.exs
+++ test/dht/bucket_test.exs
@@ -8,6 +8,7 @@ defmodule DHT.BucketTest do
     assert DHT.Bucket.cover?(bucket, 0) == true
     assert DHT.Bucket.cover?(bucket, DHT.ID.max) == true
     assert DHT.Bucket.cover?(bucket, DHT.ID.max + 1) == false
+    assert DHT.Bucket.size(bucket) == 0
   end
 
   test "#cover?/2" do
@@ -18,5 +19,26 @@ defmodule DHT.BucketTest do
     assert DHT.Bucket.cover?(bucket, 5) == true
     assert DHT.Bucket.cover?(bucket, 8) == true
     assert DHT.Bucket.cover?(bucket, 9) == false
+  end
+
+  test "#add/2 - adding a new entry to an empty bucket" do
+    bucket = %DHT.Bucket{range: 2..8}
+
+    assert DHT.Bucket.size(bucket) == 0
+    {:ok, bucket} = DHT.Bucket.add(bucket, 4)
+    assert DHT.Bucket.size(bucket) == 1
+  end
+
+  test "#add/2 - adding a new entry that is not covered by the bucket" do
+    bucket = %DHT.Bucket{range: 2..8}
+
+    assert DHT.Bucket.add(bucket, 9) == {:error, "9 is not within 2..8"}
+  end
+
+  test "#add/2 - adding an entry that already exists in the bucket" do
+    bucket = %DHT.Bucket{range: 2..8}
+    {:ok, bucket} = DHT.Bucket.add(bucket, 4)
+
+    assert DHT.Bucket.add(bucket, 4) == {:error, "4 already exists within bucket"}
   end
 end
